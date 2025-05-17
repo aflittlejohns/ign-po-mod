f88236dc0b10e2669dfd4dc167554aee200b9c73
aflittejohns <60557943+aflittlejohns@users.noreply.github.com>
2025-05-17 11:59:42 +0100

    WIP

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -1,104 +1,120 @@
-import { themes as prismThemes } from 'prism-react-renderer';
-import type { Config } from '@docusaurus/types';
-import type * as Preset from '@docusaurus/preset-classic';
+import { themes as prismThemes } from "prism-react-renderer";
+import type { Config } from "@docusaurus/types";
+import type * as Preset from "@docusaurus/preset-classic";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
 
 const config: Config = {
-  title: 'HMI Perspective Components Module',
-  tagline: 'A module for standardising HMIs',
-  favicon: 'img/favicon.ico',
+	title: "Ignition Perspective HMI Components Module",
+	tagline: "A module of HMI components for the Dairy Industry",
+	favicon: "img/favicon.ico",
 
-  // Set the production url of your site here
-  url: 'https://aflittlejohns.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/ign-po-mod/',
+	// Set the production url of your site here
+	url: "https://aflittlejohns.github.io",
+	// Set the /<baseUrl>/ pathname under which your site is served
+	// For GitHub pages deployment, it is often '/<projectName>/'
+	baseUrl: "/ign-po-mod/",
 
-  // GitHub pages deployment config.
-  // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'aflittlejohns', // Usually your GitHub org/user name.
-  projectName: 'ign-po-mod', // Usually your repo name.
+	// GitHub pages deployment config.
+	// If you aren't using GitHub pages, you don't need these.
+	organizationName: "aflittlejohns", // Usually your GitHub org/user name.
+	projectName: "ign-po-mod", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+	onBrokenLinks: "throw",
+	onBrokenMarkdownLinks: "warn",
 
-  // Even if you don't use internationalization, you can use this field to set
-  // useful metadata like html lang. For example, if your site is Chinese, you
-  // may want to replace "en" with "zh-Hans".
-  i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
-  },
+	// Even if you don't use internationalization, you can use this field to set
+	// useful metadata like html lang. For example, if your site is Chinese, you
+	// may want to replace "en" with "zh-Hans".
+	i18n: {
+		defaultLocale: "en",
+		locales: ["en"],
+	},
 
-  presets: [
-    [
-      'classic',
-      {
-        svgr: {
-          svgrConfig: {
-            icon: true,
-            expandProps: false,
-          }
-        },
-        docs: {
-          sidebarPath: './sidebars.ts',
-          routeBasePath: '/',
-          // Please change this to your repo.
-          // Remove this to remove the "edit this page" links.
-          //   editUrl:
-          //     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        blog: false,
-      },
-    ]
-  ],
-  markdown: {
-    mermaid: true
-  },
+	presets: [
+		[
+			"classic",
+			{
+				svgr: {
+					svgrConfig: {
+						icon: true,
+						expandProps: false,
+					},
+				},
+				docs: {
+					sidebarPath: "./sidebars.ts",
+					routeBasePath: "/docs",
+					// Please change this to your repo.
+					// Remove this to remove the "edit this page" links.
+					//   editUrl:
+					//     'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+				},
+				blog: false,
+				pages: {
+					path: "src/pages",
+					routeBasePath: "/",
+					include: ["**/*.{js,jsx,ts,tsx,md,mdx}"],
+					exclude: [
+						"**/_*.{js,jsx,ts,tsx,md,mdx}",
+						"**/_*/**",
+						"**/*.test.{js,jsx,ts,tsx}",
+						"**/__tests__/**",
+					],
+					mdxPageComponent: "@theme/MDXPage",
+					rehypePlugins: [],
+					beforeDefaultRemarkPlugins: [],
+					beforeDefaultRehypePlugins: [],
+				},
+			},
+		],
+	],
+	markdown: {
+		mermaid: true,
+	},
 
-  themes: ['@docusaurus/theme-mermaid',
-    [
-      require.resolve("@easyops-cn/docusaurus-search-local"),
-      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
-      ({
-        // ... Your options.
-        // `hashed` is recommended as long-term-cache of index file is possible.
-        hashed: true,
-        indexBlog: false,
-        docsRouteBasePath: '/',
-        // For Docs using Chinese, it is recomended to set:
-        // language: ["en", "zh"],
+	themes: [
+		"@docusaurus/theme-mermaid",
+		[
+			require.resolve("@easyops-cn/docusaurus-search-local"),
+			/** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+			{
+				// ... Your options.
+				// `hashed` is recommended as long-term-cache of index file is possible.
+				hashed: true,
+				indexBlog: false,
+				docsRouteBasePath: "/docs",
+				// For Docs using Chinese, it is recomended to set:
+				// language: ["en", "zh"],
 
-        // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
-        // forceIgnoreNoIndex: true,
-      }),
-    ],],
+				// If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+				// forceIgnoreNoIndex: true,
+			},
+		],
+	],
 
-  themeConfig: {
-    // Replace with your project's social card
-    navbar: {
-      title: 'ign-po-mod',
-      logo: {
-        alt: 'Ignition Logo',
-        src: 'img/Logo-Ignition-Check.svg',
-      },
-      items: [
+	themeConfig: {
+		// Replace with your project's social card
+		navbar: {
+			title: "ign-po-mod",
+			logo: {
+				alt: "Ignition Logo",
+				src: "img/Logo-Ignition-Check.svg",
+			},
+			items: [
+				{
+					href: "https://github.com/aflittlejohns/ign-po-mod/",
+					label: "GitHub",
+					position: "right",
+				},
+			],
+		},
 
-        {
-          href: 'https://github.com/aflittlejohns/ign-po-mod/',
-          label: 'GitHub',
-          position: 'right',
-        },
-      ],
-    },
-
-    prism: {
-      additionalLanguages: ['java'],
-      theme: prismThemes.github,
-      darkTheme: prismThemes.dracula,
-    },
-  } satisfies Preset.ThemeConfig,
+		prism: {
+			additionalLanguages: ["java"],
+			theme: prismThemes.github,
+			darkTheme: prismThemes.dracula,
+		},
+	} satisfies Preset.ThemeConfig,
 };
 
 export default config;
---
+++ docs/src/components/HomepageFeatures/index.js
@@ -0,0 +1,91 @@
+import clsx from "clsx";
+import Heading from "@theme/Heading";
+import styles from "./styles.module.css";
+import Link from "@docusaurus/Link";
+
+const FeatureList = [
+	{
+		title: "Easy to Use",
+		Svg: require("@site/static/img/Logo-Ignition-Check.svg").default,
+		Icon: require("@site/static/img/door-enter.svg").default,
+		description: (
+			<>
+				Guide which demonstrates how to create
+				custom components for Ignition Perspective, Inductive Automation's
+				web-based application platform. Authored by Keith Gamble.
+			</>
+		),
+		link: "/docs/keith-gamble-example-docs",
+	},
+	{
+		title: "Focus on What Matters",
+		Svg: require("@site/static/img/Logo-Ignition-Check.svg").default,
+		Icon: require("@site/static/img/door-enter.svg").default,
+		description: (
+			<>
+				A user guide for the  HMI Components interface
+				with process controllers to provide, visualisation and control.
+			</>
+		),
+		link: "/docs/aflittlejohns-docs",
+	},
+	{
+		title: "Powered by React",
+		Svg: require("@site/static/img/Logo-Ignition-Check.svg").default,
+		Icon: require("@site/static/img/door-enter.svg").default,
+		description: (
+			<>
+				A resource of HMI Component Development Workflows.
+			</>
+		),
+		link: "/docs/aflittlejohns-docs",
+	},
+	{
+		title: "Perspective-Client API",
+		Svg: require("@site/static/img/Logo-Ignition-Check.svg").default,
+		Icon: require("@site/static/img/door-enter.svg").default,
+		description: (
+			<>
+				Documentation for the "Perspective Client API" Node Package.
+			</>
+		),
+		link: "/docs/perspective-client-api/docs/",
+	},
+];
+
+function Feature({ Svg,Icon, title, description ,link}) {
+	return (
+
+		<div className={clsx("col col--4 text--center")}>
+			<div className="text--center">
+				<Svg className={styles.featureSvg} role="img" />
+			</div>
+			<div className="text--center padding-horiz--md">
+				<Heading as="h3">{title}</Heading>
+				<p style={{ textWrap: 'auto', fontSize: 'small' }}>{description}</p>
+			</div>
+			<div className={styles.buttons}>
+				<Link
+					className="button button--primary button--lg"
+					to={link}>
+				<Icon className={styles.featureSvg} role="img" />
+				</Link>
+			</div>
+		</div>
+
+	);
+}
+
+export default function HomepageFeatures() {
+	return (
+		<section className={styles.features}>
+			<div className="container">
+				<div className="row">
+					{FeatureList.map((props, idx) => (
+						<Feature key={idx} {...props} />
+					))}
+				</div>
+			</div>
+		</section>
+	);
+}
